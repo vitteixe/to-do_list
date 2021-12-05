26be113e1e32ca47dd77cac760c6393400a6e45c
Vitor Teixeira <vitoroliv@outlook.com>
2021-12-05 16:43:07 -0300

    Ínicio do app

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-do List
+
+To-do List é um aplicativo com objetivo de você criar uma lista de tarefas e organiza-las do seu jeito.
---
+++ android/app/src/main/kotlin/vitteixe/dev/todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package vitteixe.dev.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
